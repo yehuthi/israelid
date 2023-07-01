c99b4ce21e1a82720a12641165dbdaf35c34fa4d
Yehuthi <yehuthith@gmail.com>
2023-07-01 09:11:23 +0300

    ⌚ improve bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,11 +1,18 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+#[no_mangle]
+#[inline(never)]
+pub fn valid_bench() { israelid::valid_ascii(black_box(*b"123456782")); }
+
+#[no_mangle]
+#[inline(never)]
+pub fn checksum_bench() { israelid::checksum_digit_ascii(black_box(*b"12345678")); }
+
 fn bench(c: &mut Criterion) {
 	c.bench_function("valid", |b| {
-		b.iter(|| {
-			israelid::valid_ascii(black_box(*b"123456782"));
-		});
+		b.iter(valid_bench);
 	});
+	c.bench_function("checksum", |b| b.iter(checksum_bench));
 }
 
 criterion_group!(benches, bench);
