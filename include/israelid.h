cdda42d79408450d274a075ab966c88703aac656
Yehuthi <yehuthith@gmail.com>
2024-04-19 23:24:49 +0300

    document header

--- include/israelid.h
+++ include/israelid.h
@@ -7,14 +7,23 @@ extern "C" {
 #include <stdint.h>
 
 enum {
+	/// The length of Israeli ID numbers (including the control digit).
 	ISRAELID_ID_LEN = 9
 };
 
+/// The integer type for checksums.
 typedef uint8_t israelid_checksum_t;
 
+/// Computes the checksum for the given Israeli ID number.
+/// The input length to this function doesn't have to be ::ISRAELID_ID_LEN.
 israelid_checksum_t israelid_checksum_ascii(const char *id, uint8_t len);
+
+/// Checks whether the given checksum is valid.
+/// Note that the length of the ID is not manifested in the checksum, therefore, a checksum may be valid but the ID number invalid if the length is not ::ISRAELID_ID_LEN.
+/// See ::israelid_valid_ascii, which checks both the length and the checksum.
 bool israelid_checksum_valid(israelid_checksum_t checksum);
 
+/// Checks whether the given Israeli ID number is valid.
 bool israelid_valid_ascii(const char *id, uint8_t len);
 
 // internal (for testing and benchmarking):
