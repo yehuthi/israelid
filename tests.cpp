73939a823010a7585f85a5d6e827008f11a5e135
yehuthi <yehuthith@gmail.com>
2024-04-19 20:16:38 +0300

    add tests

--- tests.cpp
+++ tests.cpp
@@ -20,3 +20,26 @@ TEST(israelid, checksum_ascii_sse) {
 		_israelid_checksum_ascii_scalar(input, 9)
 	);
 }
+
+TEST(israelid, valid) {
+	// 123..
+	EXPECT_FALSE(israelid_valid_ascii("123456789", 9));
+	EXPECT_TRUE(israelid_valid_ascii("123456782", 9));
+	// good
+	EXPECT_TRUE(israelid_valid_ascii("654223999", 9));
+	EXPECT_TRUE(israelid_valid_ascii("000000000", 9));
+	EXPECT_TRUE(israelid_valid_ascii("111111118", 9));
+	EXPECT_TRUE(israelid_valid_ascii("222222226", 9));
+	EXPECT_TRUE(israelid_valid_ascii("333333334", 9));
+	EXPECT_TRUE(israelid_valid_ascii("444444442", 9));
+	// bad
+	EXPECT_FALSE(israelid_valid_ascii("654223991", 9));
+	EXPECT_FALSE(israelid_valid_ascii("999999999", 9));
+	EXPECT_FALSE(israelid_valid_ascii("555555550", 9));
+	// bad (misc)
+	EXPECT_FALSE(israelid_valid_ascii("", 0));
+	EXPECT_FALSE(israelid_valid_ascii("1", 1));
+	EXPECT_FALSE(israelid_valid_ascii("12", 2));
+	EXPECT_FALSE(israelid_valid_ascii("1abc5", 5));
+
+}
