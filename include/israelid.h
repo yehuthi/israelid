ba46f00ccf7178353f4c6771155da239d64472d8
yehuthi <yehuthith@gmail.com>
2024-04-20 20:16:52 +0300

    improve docs formatting

--- include/israelid.h
+++ include/israelid.h
@@ -16,10 +16,12 @@ enum {
 typedef uint8_t israelid_checksum_t;
 
 /// Computes the checksum for the given Israeli ID number.
+///
 /// The input length to this function doesn't have to be ::ISRAELID_ID_LEN.
 israelid_checksum_t israelid_checksum_ascii(const char *id, uint8_t len);
 
 /// Checks whether the given checksum is valid.
+///
 /// Note that the length of the ID is not manifested in the checksum, therefore, a checksum may be valid but the ID number invalid if the length is not ::ISRAELID_ID_LEN.
 /// See ::israelid_valid_ascii, which checks both the length and the checksum.
 bool israelid_checksum_valid(israelid_checksum_t checksum);
@@ -28,6 +30,7 @@ bool israelid_checksum_valid(israelid_checksum_t checksum);
 bool israelid_valid_ascii(const char *id, uint8_t len);
 
 /// Computes the checksum digit for an ID.
+///
 /// That is: the ::israelid_checksum_ascii of the string `"<id><israelid_id_checksum(id)>"` should always be a ::israelid_checksum_valid.
 uint8_t israelid_id_checksum(const char* id, uint8_t len);
 
